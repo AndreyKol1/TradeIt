4dde0dc5c74b9bb4b45c4158ac6da61704929339
dornierdo17 <koliush2006@gmail.com>
2025-12-02 13:04:54 +0100

    Add SQL commands to create tables

---
+++ app/database/tables.py
@@ -0,0 +1,28 @@
+
+def create_crypto_news_table():
+    return """CREATE TABLE IF NOT EXISTS crypto_news(
+                        id SERIAL PRIMARY KEY,
+                        title TEXT NOT NULL,
+                        summary TEXT NOT NULL,
+                        publish_time DATE NOT NULL,
+                        currency VARCHAR(3) NOT NULL,
+                        relevance_score  REAL NOT NULL,
+                        ticker_sentiment REAL NOT NULL,
+
+                        CONSTRAINT unique_news_item UNIQUE (currency, title, publish_time)
+                        )"""
+
+def create_crypto_prices_table():
+        return """CREATE TABLE IF NOT EXISTS crypto_prices(
+                        id SERIAL PRIMARY KEY,
+                        price_date DATE NOT NULL,
+                        currency VARCHAR(3) NOT NULL,
+                        time_period TEXT NOT NULL,
+                        open_price REAL NOT NULL,
+                        highest_price REAL NOT NULL,
+                        lowest_price REAL NOT NULL,
+                        close_price REAL NOT NULL,
+                        volume REAL NOT NULL,
+
+                        CONSTRAINT unique_price_item UNIQUE (price_date, currency, time_period)
+                        )"""
