c9077f20e96ea93fd17da82763c8819ad05ee459
dornierdo17 <koliush2006@gmail.com>
2025-12-02 13:15:44 +0100

    Move table creating logic to table.py. Add prices table creation

--- app/database/lifespan.py
+++ app/database/lifespan.py
@@ -2,8 +2,10 @@ from contextlib import  asynccontextmanager
 from app.database.config import CONN_STRING 
 from app.utils.logger import get_logger
 import psycopg
+from app.database.tables import create_crypto_news_table, create_crypto_prices_table
 
 logger = get_logger("main")
+
 
 @asynccontextmanager
 async def lifespan(app):
@@ -11,21 +13,11 @@ async def lifespan(app):
     try:
         async with await psycopg.AsyncConnection.connect(CONN_STRING) as conn: # creating a databse instance
             async with conn.cursor() as cur: # creating a cursor with which we can query the db
-                await cur.execute("""
-                    CREATE TABLE IF NOT EXISTS crypto_news(
-                        id SERIAL PRIMARY KEY,
-                        title TEXT NOT NULL,
-                        summary TEXT NOT NULL,
-                        publish_time DATE NOT NULL,
-                        ticker VARCHAR(20) NOT NULL,
-                        relevance_score  REAL NOT NULL,
-                        ticker_sentiment REAL NOT NULL,
-
-                        CONSTRAINT unique_news_item UNIQUE (ticker, title, publish_time)
-                        )
-                    """)
+                await cur.execute(create_crypto_news_table())
+                await cur.execute(create_crypto_prices_table())
+                    
                 await conn.commit() # Make the changes to the database persistent
-                logger.info("Startup: Table 'news_db' is ready.")
+                logger.info("Startup: Tables 'news_db, crypto_prices' are ready.")
 
     except Exception as e:
         logger.error(f"Startup Error: Could not connect to DB. {e}")
