eb24757e0d799017299884f50ad4951389a2ae30
dornierdo17 <koliush2006@gmail.com>
2025-12-02 12:57:50 +0100

    Add pydantic schema for secrets

---
+++ app/schemas/env_schema.py
@@ -0,0 +1,15 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    POSTGRES_USER: str 
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str 
+    POSTGRES_HOST: str 
+    POSTGRES_PORT: int
+    CRYPTO_API_KEY: str 
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+settings = Settings()
+
+
