96ef70d6bd3ec2a624950c06ca64c8eed17b30b5
dornierdo17 <koliush2006@gmail.com>
2025-12-02 13:13:52 +0100

    Rename ticker to currency

--- app/api/fetch_news.py
+++ app/api/fetch_news.py
@@ -2,23 +2,24 @@ from fastapi import APIRouter, Depends
 from app.dependencies import get_conn
 
 from app.dependencies import get_news_fetcher
+from typing import Dict
 
 
 router = APIRouter()
 
 @router.post("/get_news")
-async def add_news(currency: str, conn=Depends(get_conn)):
+async def add_news(currency: str, conn=Depends(get_conn)) -> Dict:
     news_class_cached = get_news_fetcher()
     news = await news_class_cached.fetch_news(currency)
     async with conn.cursor() as cursor:
         for doc in news:
             await cursor.execute("""INSERT INTO crypto_news
-                                    (title, summary, publish_time, ticker, relevance_score, ticker_sentiment)
+                                    (title, summary, publish_time, currency, relevance_score, ticker_sentiment)
                                     VALUES (%s, %s, %s, %s, %s, %s)
-                                    ON CONFLICT (ticker, title, publish_time) DO NOTHING""",
+                                    ON CONFLICT (currency, title, publish_time) DO NOTHING""",
                                     
                                     (doc.title, doc.summary, doc.date_published,
-                                     doc.ticker, doc.relevance_score, doc.ticker_sentiment_score))
+                                     doc.currency, doc.relevance_score, doc.ticker_sentiment_score))
 
         await conn.commit()
 
