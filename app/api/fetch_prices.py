fddf946362127acb1d446019d1250517a8b9d54c
dornierdo17 <koliush2006@gmail.com>
2025-12-04 19:20:03 +0100

    Rewrite to have only enpoint layer logic

--- app/api/fetch_prices.py
+++ app/api/fetch_prices.py
@@ -1,7 +1,9 @@
+from app.repositories.prices_repository import insert_prices
 from fastapi import APIRouter, Depends
-from app.dependencies import get_conn
 
+from app.dependencies import get_conn
 from app.dependencies import get_price_fetcher
+
 from typing import Dict
 
 router = APIRouter()
@@ -10,19 +12,8 @@ router = APIRouter()
 async def add_prices(currency: str, period: str = "DAILY", conn=Depends(get_conn)) -> Dict:
     price_class_cached = get_price_fetcher()
     prices = await price_class_cached.fetch_prices(currency, period)
-    async with conn.cursor() as cursor:
-        for doc in prices:
-            await cursor.execute("""INSERT INTO crypto_prices
-                                    (price_date, currency, time_period, open_price, highest_price,
-                                    lowest_price, close_price, volume)
 
-                                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-                                    ON CONFLICT (price_date, currency, time_period) DO NOTHING""",
-                                    
-                                    (doc.date_price, doc.currency, doc.time_period, doc.open_price, doc.high,
-                                     doc.low, doc.close, doc.volume))
-
-        await conn.commit()
+    await insert_prices(conn, prices)
 
 
     return {"message": f"Successfully fetched prices for {currency}!"}
