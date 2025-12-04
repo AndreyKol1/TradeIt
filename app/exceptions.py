a0fa68943100ce4c8e3930062570e3a5f99d17b6
dornierdo17 <koliush2006@gmail.com>
2025-12-04 19:09:32 +0100

    Implement custom exceptions for API calls

---
+++ app/exceptions.py
@@ -0,0 +1,15 @@
+
+class ExternalAPIError(Exception):
+    def __init__(self, service: str, message: str):
+        self.service = service 
+        self.message = message 
+        super().__init__(f"{service}: {message}")
+
+class DataParsingError(Exception):
+    """Failure in parsing argument from extracted data"""
+
+    def __init__(self, message: str):
+        self.message = message
+        super().__init__(message)
+
+    
